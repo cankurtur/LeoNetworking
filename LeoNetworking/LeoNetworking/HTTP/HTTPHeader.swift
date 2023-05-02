e680c0b0bf8d40ea6e0403a8001f93c2d45aa960
cankurtur <cankurtur@gmail.com>
2023-05-02 23:54:20 +0300

    delete unnecessary spaces

--- LeoNetworking/LeoNetworking/HTTP/HTTPHeader.swift
+++ LeoNetworking/LeoNetworking/HTTP/HTTPHeader.swift
@@ -11,14 +11,14 @@ public struct HTTPHeader {
     
     /// Name of the header.
     public let name: String
-    
+
     /// Value of the header.
     public let value: String
     
-    /// Creates an instance from the given `name` and `value`
+    /// Creates an instance from the given `name` and `value`.
     ///
     /// - Parameters:
-    ///   - name: The name of the header.
+    ///   - name:  The name of the header.
     ///   - value: The value of the header.
     public init(name: String, value: String) {
         self.name = name
