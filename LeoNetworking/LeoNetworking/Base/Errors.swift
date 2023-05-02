1d3a61419d868af3120d3471a9ab72ea3d4e1a20
cankurtur <cankurtur@gmail.com>
2023-05-02 23:53:12 +0300

    crate endpoint protocol

---
+++ LeoNetworking/LeoNetworking/Base/Errors.swift
@@ -0,0 +1,76 @@
+//
+//  Errors.swift
+//  LeoNetworking
+//
+//  Created by Can Kurtur on 29.04.2023.
+//
+import Foundation
+
+/// Generic error type for error layer, provide your own type with confirming this protocol
+/// to be able to cast network error to your own error model.
+public protocol APIError: Codable, AnyObject {
+    var message: String { get }
+    var statusCode: Int? { get set }
+}
+
+/// Concrete API error type.
+public enum APIClientError: Error {
+    case handledError(error: APIError)
+    case networkError
+    case decoding(error: DecodingError?)
+    case timeout
+    case invalidStatusCode
+    case badRequest
+    
+    public var message: String {
+        switch self {
+        case .handledError(let error):
+            return error.message
+        case .decoding:
+            return "Decoding Error"
+        case .networkError:
+            return "Connection Error"
+        case .timeout:
+            return "Timeout"
+        case .invalidStatusCode:
+            return "Invalid status code"
+        case .badRequest:
+            return "Bad request"
+        }
+    }
+
+    public var debugMessage: String {
+        switch self {
+        case .handledError(let error):
+            return error.message
+        case .networkError:
+            return "Network error"
+        case .decoding(let decodingError):
+            guard let decodingError = decodingError else { return "Decoding Error" }
+            return "\(decodingError)"
+        case .timeout:
+            return "Timeout"
+        case .invalidStatusCode:
+            return "Invalid status code"
+        case .badRequest:
+            return "Bad request"
+        }
+    }
+    
+    public var statusCode: Int {
+        switch self {
+        case .handledError(let error):
+            return error.statusCode ?? 0
+        case .networkError:
+            return 400
+        case .decoding:
+            return NSURLErrorCannotDecodeRawData
+        case .timeout:
+            return NSURLErrorTimedOut
+        case .invalidStatusCode:
+            return 0
+        case .badRequest:
+            return 0
+        }
+    }
+}
