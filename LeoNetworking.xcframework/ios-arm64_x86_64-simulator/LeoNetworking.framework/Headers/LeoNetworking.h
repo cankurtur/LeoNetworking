dfac561f42ba30acafa95011af9beafb31773bc2
cankurtur <cankurtur@gmail.com>
2023-05-05 16:02:45 +0300

    create new xcframework

---
+++ LeoNetworking.xcframework/ios-arm64_x86_64-simulator/LeoNetworking.framework/Headers/LeoNetworking.h
@@ -0,0 +1,18 @@
+//
+//  LeoNetworking.h
+//  LeoNetworking
+//
+//  Created by Can Kurtur on 29.04.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LeoNetworking.
+FOUNDATION_EXPORT double LeoNetworkingVersionNumber;
+
+//! Project version string for LeoNetworking.
+FOUNDATION_EXPORT const unsigned char LeoNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeoNetworking/PublicHeader.h>
+
+
